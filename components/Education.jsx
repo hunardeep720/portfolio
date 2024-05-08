e07ebc2b5bc663201cb27da5a40dcd3a3fc79658
hunardeep720 <123598156+hunardeep720@users.noreply.github.com>
2024-05-07 23:06:58 -0600

    changes

--- components/Education.jsx
+++ components/Education.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 function Education() {
   return (
-    <div id='education' className='text-center max-w-screen-xl mx-auto m-10 gap-5 text-black grid grid-cols-3 bg-white pt-24 sm:pt-40'>
-        <p className='font-extrabold text-2xl col-span-2'>SOUTHERN ALBERTA INSTITUTE OF TECHNOLOGY (SAIT)</p>
+    <div id='education' className='text-center max-w-screen-xl mx-auto m-10 gap-5 text-black grid sm:grid-cols-3 grid-cols-1 px-6 bg-white pt-24 sm:pt-40 justify-center items-center'>
+        <p className='col-span-full text-3xl font-extrabold w-1/2 border-black border-b-2 mx-auto mb-10'>Education</p>
+        <p className='font-bold text-2xl col-span-2'>SOUTHERN ALBERTA INSTITUTE OF TECHNOLOGY (SAIT)</p>
         <p className='text-xl font-bold'>2022-2024</p>
         <div className='col-span-full'>
             <div className='grid-cols-2'>
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,17 +3,39 @@ import Link from "next/link";
 
 export default function Hero() {
   return (
-    <div id="home" className=" max-w-screen-xl mx-auto m-10 grid grid-cols-1 sm:grid-cols-2 gap-5 text-black bg-white pt-24 sm:pt-40">
+    <div
+      id="home"
+      className=" max-w-screen-xl mx-auto m-10 grid grid-cols-1 sm:grid-cols-2 gap-5 text-black bg-white pt-24 sm:pt-40 items-center justify-center text-center"
+    >
       <div className="flex justify-center items-center text-center">
-        <p className="rounded-full sm:w-60 sm:h-60 h-32 w-32 border-black border-4 text-center">hbeufbuebf</p>
+        <p className="rounded-full sm:w-60 sm:h-60 h-32 w-32 border-black border-4 text-center">
+          Photo
+        </p>
       </div>
-      <div className="text-center grid items-center">
-        <h1 className="text-2xl font-extrabold sm:text-4xl">Hi, I'm Hunardeep Singh</h1>
-        <p className="text-center font-bold text-xl">I'm a software engineer</p>
-        <div className="col-span-full">
-          <Link href={`tel:${8254888028}`}>
-            <p>(825)-488-8028</p>
+      <div className="text-center grid items-center justify-center w-full" >
+        <h1 className="text-2xl font-extrabold sm:text-4xl text-center">
+          Hi, I'm Hunardeep Singh
+        </h1>
+        <p className="text-center font-bold text-xl mt-2">I'm a software engineer</p>
+        <div className="mt-8 col-span-full flex flex-col">
+          <Link href={`tel:8254888028`}>
+            <p className="font-semibold hover:text-slate-400">(825)-488-8028</p>
           </Link>
+          <Link href={`mailto:dhillonhunar@gmail.com`} className="my-3 font-semibold hover:text-slate-400">
+            DHILLONHUNAR@GMAIL.COM
+          </Link>
+          <div>
+            <Link href="https://github.com/hunardeep720" target="_blank" className="pr-3 border-r-2 border-black font-semibold hover:text-slate-400">
+              GitHub
+            </Link>
+            <Link
+              href="https://www.linkedin.com/in/hunardeep-singh-9a5785269?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BbGDtpMeVRRyJpG107HUDsA%3D%3D"
+              target="_blank"
+              className="pl-3 font-semibold hover:text-slate-400"
+            >
+              LinkedIn
+            </Link>
+          </div>
         </div>
       </div>
       {/* <hr className="col-span-full my-10 sm:my-36 border-black sm:border-2" /> */}
