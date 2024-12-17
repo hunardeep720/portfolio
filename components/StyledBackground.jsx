dc0d6a5d92a947e7b719b66e24da538fb22c60a1
hunardeep720 <hunardeepsingh@edu.sait.ca>
2024-12-16 18:49:23 -0700

    update background according to the screen sizw

--- components/StyledBackground.jsx
+++ components/StyledBackground.jsx
@@ -5,18 +5,27 @@ import React, { useState, useEffect } from "react";
 export default function StyledBackground() {
   const [binaryStreams, setBinaryStreams] = useState([]);
 
-  // Generate binary streams only once on the client side
   useEffect(() => {
-    const streams = Array.from({ length: 50 }).map(() => ({
-      left: `${Math.random() * 100}vw`,
-      top: `${-100 + Math.random() * 200}vh`,
-      animationDuration: `${5 + Math.random() * 5}s`,
-      animationDelay: `${Math.random() * 5}s`,
-      opacity: `${0.5 + Math.random() * 0.5}`,
-      content: Array.from({ length: 100 })
-        .map(() => (Math.random() > 0.5 ? "0" : "1"))
-        .join(""),
-    }));
+    // Function to determine multiplier based on screen size
+    const getMultiplier = () => {
+      return window.innerWidth <= 768 ? 10 : 30; // 10 for phone, 30 for laptop
+    };
+
+    // Generate binary streams only once on the client side
+    const streams = Array.from({ length: 50 }).map(() => {
+      const multiplier = getMultiplier();
+      return {
+        left: `${Math.random() * multiplier}vw`,
+        top: `${-100 + Math.random() * 200}vh`,
+        animationDuration: `${5 + Math.random() * 5}s`,
+        animationDelay: `${Math.random() * 5}s`,
+        opacity: `${0.5 + Math.random() * 0.5}`,
+        content: Array.from({ length: 100 })
+          .map(() => (Math.random() > 0.5 ? "0" : "1"))
+          .join(""),
+      };
+    });
+
     setBinaryStreams(streams);
   }, []);
 
