6ba8ab90f77f3faacedff9c20a9ff33c2c43f733
hunardeep720 <hunardeepsingh@edu.sait.ca>
2024-12-26 12:41:53 -0700

    update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "thebrdge.com",
+        },
+        {
+            protocol: "https",
+            hostname: "studentscoops.vercel.app",
+          },
+      ],
+    },
+    // other Next.js config options can be added here
+  };
 
 export default nextConfig;
